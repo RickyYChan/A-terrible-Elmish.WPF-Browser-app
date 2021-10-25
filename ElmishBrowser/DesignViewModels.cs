30ecaf3be4c4d512121cbd90228b070c082da969
ricky <ricky.y.c@outlook.com>
2021-10-25 23:01:47 +0800

    add design vm

--- ElmishBrowser/DesignViewModels.cs
+++ ElmishBrowser/DesignViewModels.cs
@@ -4,6 +4,7 @@ namespace ElmishBrowser
 {
     public static class DesignViewModels
     {
-
+        public static dynamic MainWindowDesignVm { get; } =
+            Program.getDesignVm(Program.App.init, Program.App.bindings());
     }
 }
