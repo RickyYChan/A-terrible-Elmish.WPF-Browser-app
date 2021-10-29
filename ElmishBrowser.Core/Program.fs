da24fb0c3e9c5f701c0bc7a9abb999ffc064f259
ricky <ricky.y.c@outlook.com>
2021-10-29 20:44:30 +0800

    add webview and rm 'begin end'

--- ElmishBrowser.Core/Program.fs
+++ ElmishBrowser.Core/Program.fs
@@ -4,6 +4,7 @@ open Serilog
 open Serilog.Extensions.Logging
 open Elmish.WPF
 open Elmish
+open System.Windows
 
 [<AutoOpen>]
 module IList = 
@@ -11,24 +12,27 @@ module IList =
     let len = Seq.length
     let add d (x:ImmutableList<_>) = x.Add d 
     let rm ind (x:ImmutableList<_>) = x.RemoveAt ind 
+    let init = ImmutableList.CreateRange
 
 [<AutoOpen>]
 /// use static classes: System.Windows.Media.Brushes and System.Windows.Application.    
 /// can use mkProgramWithCmdMsg as an alternative.   
 /// seems dirty.  
-module FWColor = begin 
+module FWColor = 
     type Color = System.Windows.Media.Brushes
     let accent = System.Windows.Application.Current.Resources.["ImmersiveSystemAccentBrush"]
     let transparent = Color.Transparent :> obj 
-    end 
 
-module Tab = begin
+
+[<AutoOpen>]
+module Tab =
     type TabItem = 
         {
             Content: obj 
+            WvVisibility: Visibility
         }
-    end 
-module App = begin
+
+module App = 
     open System.Collections.Immutable
     type Model =
         { 
@@ -37,37 +41,42 @@ module App = begin
         }
     
     type Msg =
-        | AddTab
+        //| AddTab
         | Select of int
     
     let init =
         {
-            Tabs = ImmutableList.Empty
+            Tabs = { 0 .. 5 } |> Seq.map(fun x -> { Content = $"Hello {x}"; WvVisibility = Visibility.Hidden }) |> IList.init
             SelectedId = -1
         }, Cmd.none
     
     let update msg m = 
         match msg with 
-        | AddTab -> { m with Tabs = m.Tabs |> IList.add { Content = $"Hello {m.Tabs |> len}" } }, 
-                    Cmd.ofMsg (Select m.Tabs.Count)
+        //| AddTab -> { m with Tabs = m.Tabs |> IList.add { Content = $"Hello {m.Tabs |> len}" } }, 
+        //            Cmd.ofMsg (Select m.Tabs.Count)
         | Select i -> { m with SelectedId = i }, Cmd.none
     
     let bindings () : Binding<Model, Msg> list = 
         [
-            "AddTabCmd" |> Binding.cmd AddTab
+            //"AddTabCmd" |> Binding.cmd AddTab
             "TabSource" |> Binding.subModelSeq(
                 (fun m -> m.Tabs |> Seq.indexed), 
                 (fun (i, t) -> i), 
-                (fun () -> 
-                    [
+                (fun () ->  [
                         "TabContent" |> Binding.oneWay(fun (m, (i, t)) -> t.Content) 
                         "SelectItem" |> Binding.cmd(fun (m, (i, t)) -> Select i)
                         "TabBg" |> Binding.oneWay(fun (m, (i, t)) -> 
                             if i = m.SelectedId then accent else transparent)
-                    ])
-            )
+                    ]))
+            
+            "ViewBorder" |> Binding.subModelSeq(
+                (fun m -> m.Tabs |> Seq.indexed),
+                (fun (i, v) -> i),
+                (fun () -> [
+                    "WvVisibility" |> Binding.oneWay(fun (m, (i, t)) -> t.WvVisibility)
+                ]))
         ]
-    end 
+
 
 let getDesignVm (m:'model, b:Binding<'model, 'msg> list) = ViewModel.designInstance m b
 let main window =
