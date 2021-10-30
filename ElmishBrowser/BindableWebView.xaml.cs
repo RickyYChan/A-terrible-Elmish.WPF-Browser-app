d7c5f7f0615f1d9479f79a8c6f49283871936abc
ricky <ricky.y.c@outlook.com>
2021-10-30 19:20:50 +0800

    bug

---
+++ ElmishBrowser/BindableWebView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ElmishBrowser
+{
+    public partial class BindableWebView : UserControl
+    {
+        public BindableWebView()
+        {
+            InitializeComponent(); // init 
+
+            DependencyPropertyDescriptor.FromProperty(IsDisposedProperty, typeof(BindableWebView))
+                .AddValueChanged(this, (sender, e) =>
+                {
+                    if (IsDisposed == true)
+                    {
+                        System.Console.WriteLine("\n[[[ Dispoing! ]]]\n");
+                        selfWv.Dispose();
+                    }
+                }); // regist property changed event of a denpendency property. 
+
+            selfWv.CoreWebView2InitializationCompleted += (sender, e) =>
+                selfWv.CoreWebView2.NewWindowRequested += (sender, e) =>
+                    e.NewWindow = selfWv.CoreWebView2;
+        }
+        public string Address
+        {
+            get { return (string)GetValue(AddressProperty); }
+            set { SetValue(AddressProperty, value); }
+        }
+        public static readonly DependencyProperty AddressProperty =
+            DependencyProperty.Register("Address", typeof(string), typeof(BindableWebView), new PropertyMetadata("https://www.bing.com"));
+        public bool IsDisposed
+        {
+            get { return (bool)GetValue(IsDisposedProperty); }
+            set { SetValue(IsDisposedProperty, value); }
+        }
+        public static readonly DependencyProperty IsDisposedProperty =
+            DependencyProperty.Register("IsDisposed", typeof(bool), typeof(BindableWebView), new PropertyMetadata(false));
+    }
+}
--- ElmishBrowser/MainWindow.xaml.cs
+++ ElmishBrowser/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Web.WebView2.Wpf;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ElmishBrowser
 {
@@ -8,13 +7,6 @@ namespace ElmishBrowser
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void WebView2_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
-        {
-            var wv = sender as WebView2;
-            wv.CoreWebView2.NewWindowRequested += (sender, e) =>
-                e.NewWindow = wv.CoreWebView2;
         }
     }
 }
